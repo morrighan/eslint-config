c046699c498acef7ecafb4c987f83c998c293c50
Yongbin Min <contact@cichol.com>
2021-05-08 15:01:14 +0900

    Prepare to release `v3.0.0-beta.1`

--- rules/typescript.js
+++ rules/typescript.js
@@ -33,6 +33,7 @@ module.exports = Object.assign(mapExtensionRules({
      * @see {@link https://eslint.org/docs/rules/require-await}
      */
     'require-await': 'off'
+
 }), {
 
     /**
@@ -47,6 +48,15 @@ module.exports = Object.assign(mapExtensionRules({
             objectLiteralTypeAssertions: 'allow'
         }
     ],
+
+    /**
+     * Enforces explicit accessibility modifiers on class properties and methods.
+     *
+     * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/explicit-member-accessibility.md}
+     */
+    '@typescript-eslint/explicit-member-accessibility': [ 'error', {
+        accessibility: 'explicit'
+    } ],
 
     /**
      * Enforces a consistent member delimiter style in interfaces and type literals.
@@ -65,17 +75,29 @@ module.exports = Object.assign(mapExtensionRules({
     } ],
 
     /**
+     * Enforces a consistent member declaration order.
+     *
+     * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/member-ordering.md}
+     */
+    '@typescript-eslint/member-ordering': 'error',
+
+    /**
      * Enforces naming conventions for everything across a codebase.
      *
      * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md}
      */
-    '@typescript-eslint/naming-convention': [
-        'error', {
-            selector: 'interface',
-            format: [ 'PascalCase' ],
-            custom: { regex: '^I[A-Z]', match: false }
-        }
-    ],
+    '@typescript-eslint/naming-convention': [ 'error', {
+        selector: 'interface',
+        format: [ 'PascalCase' ],
+        custom: { regex: '^I[A-Z]', match: false }
+    } ],
+
+    /**
+     * Allows the declaration of empty interfaces.
+     *
+     * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-empty-interface.md}
+     */
+    '@typescript-eslint/no-empty-interface': 'off',
 
     /**
      * Allows to use of the `any` type.
@@ -124,5 +146,19 @@ module.exports = Object.assign(mapExtensionRules({
      *
      * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-nullish-coalescing.md}
      */
-    '@typescript-eslint/prefer-nullish-coalescing': 'error'
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+
+    /**
+     * Enforces the use of `String#startsWith` and `String#endsWith` instead of other equivalent methods of checking substrings.
+     *
+     * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-string-starts-ends-with.md}
+     */
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+
+    /**
+     * Enforces consistent spacing around type annotations.
+     *
+     * @see {@link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/type-annotation-spacing.md}
+     */
+    '@typescript-eslint/type-annotation-spacing': 'error'
 });
